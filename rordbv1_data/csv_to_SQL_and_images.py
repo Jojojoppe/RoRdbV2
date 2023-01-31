8e60d475d2b7b6bc733d9782620985e2e07ad877
Joppe Blondel <joppe@blondel.nl>
2023-01-31 12:55:40 +0100

    Scraping scripts to SQL working
    Signed-off-by: Joppe Blondel <joppe@blondel.nl>

--- rordbv1_data/csv_to_SQL_and_images.py
+++ rordbv1_data/csv_to_SQL_and_images.py
@@ -29,14 +29,71 @@ def img_to_url(img, name):
     i.close()
     return s
 
+images = {}
+categories = []
+locations = []
+items = []
 with open('rordbv1_data.csv') as csvfile:
     reader = csv.reader(csvfile)
     id = None
     for row in reader:
         if id is None:
-            id = 0
+            id = 1
             continue
         imguri = img_to_url(row[0], str(id))
+        images[id] = (imguri, row[1])
         with open(f'images/{id}.imguri', 'w') as f:
             f.write(imguri)
+
+        # Add category and location
+        if row[2] not in categories:
+            categories.append(row[2])
+        if row[3] not in locations:
+            locations.append(row[3])
+
+        items.append({
+            'name':row[1],
+            'category':categories.index(row[2])+3,
+            'location':locations.index(row[3])+3,
+            'claimedby':row[8],
+            'hidden':row[9],
+            'img':id,
+            'color':row[4],
+            'amount':row[6],
+            'size':row[5],
+            'comments':row[7],
+        })
+
         id += 1
+
+# Generate SQL
+cattable = "wp_rordbv2_cat"
+loctable = "wp_rordbv2_loc"
+imgtable = "wp_rordbv2_img"
+itemstable = "wp_rordbv2_items"
+with open('rordbv1_catloc.sql', 'w') as sql:
+    for cat in categories:
+        sql.write(f"INSERT INTO {cattable} (name, parentid, parentid_list, childid_list, searchtags) VALUES ('{cat}', 1, ',1,', ',', ',{cat},All,');\n")
+    for loc in locations:
+        sql.write(f"INSERT INTO {loctable} (name, parentid, parentid_list, childid_list, searchtags) VALUES ('{loc}', 1, ',1,', ',', ',{loc},All,');\n")
+
+imgfile=0
+sql = open(f"rordbv1_img{imgfile}.sql", 'w')
+nr = 0
+for img in images.values():
+    # if nr>25:
+        # nr = 0
+        # sql.close()
+        # imgfile += 1
+        # sql = open(f"rordbv1_img{imgfile}.sql", 'w')
+    sql.write(f"INSERT INTO {imgtable} (name, data) VALUES ('{img[1]}', '{img[0]}');\n")
+    nr += 1
+sql.close()
+
+with open('rordbv1_items.sql', 'w') as sql:
+    for item in items:
+        if item['name']=='':
+            continue
+        if item['img']=='':
+            continue
+        sql.write(f"INSERT INTO {itemstable} (name, category, location, claimedby, hidden, img, color, amount, size, comments) VALUES ('{item['name']}', {item['category']}, {item['location']}, '{item['claimedby']}', {item['hidden']}, {item['img']}, '{item['color']}', '{item['amount']}', '{item['size']}', '{item['comments']}');\n")
